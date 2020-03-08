13940ba605cff49e76f72e5da8705781ebee9e4e
root <root@ip-10-0-0-229.ap-southeast-1.compute.internal>
2020-03-08 16:41:29 +0000

    script is added

---
+++ javamavenjenkinstomcat8.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+apt-get update
+apt-get install git -y
+git --version
+apt-get insatll openjdk-8-jdk -y
+java  -version
+apt-get install maven -y
+mvn  --version
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install jenkins
+systemctl start jenkins
+systemctl status jenkins
+apt-get install tomcat8 -y
+service tomcat8 status
