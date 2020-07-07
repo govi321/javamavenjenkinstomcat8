bea6dc7485fe6c4ba3ce7881621c3081311454cf
govi321 <51965283+govi321@users.noreply.github.com>
2020-07-07 15:02:17 +0530

    Update javamavenjenkinstomcat8.sh
    shript was added

--- javamavenjenkinstomcat8.sh
+++ javamavenjenkinstomcat8.sh
@@ -1,16 +1,16 @@
 
 #!/bin/bash
-apt-get update
+sudo apt update
 apt-get install git -y
 git --version
-apt-get install openjdk-8-jdk -y
-java  -version
-apt-get install maven -y
-mvn  --version
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install maven -y
+ mvn --version
+sudo apt install openjdk-8-jdk -y
+java -version
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 apt-get install tomcat8 -y
